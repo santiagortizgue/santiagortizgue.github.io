1b65f32eac9f07825da05094fd1d7d8456a7a317
santiagortizgue <santiagortizgue@gmail.com>
2021-09-21 20:19:34 -0500

    fixed footer links

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <div className="Footer">
             <ProfileLinks/>
 
-            <p>Made using <a href="/" target="_blank" rel="noreferrer">React</a> & <a href="/" target="_blank" rel="noreferrer">Strapi</a></p>
+            <p>Made using <a href="https://es.reactjs.org/" target="_blank" rel="noreferrer">React</a> & <a href="https://strapi.io/" target="_blank" rel="noreferrer">Strapi</a></p>
         </div>
     );
 }
