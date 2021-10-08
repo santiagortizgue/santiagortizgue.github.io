f3934b63e101daccae5da3200b076970916c3abe
santiagortizgue <santiagortizgue@gmail.com>
2021-10-07 21:55:21 -0500

    changed web menu items logic

--- src/components/NavMenu/NavMenu.jsx
+++ src/components/NavMenu/NavMenu.jsx
@@ -1,31 +1,60 @@
 import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import { useLocation, useHistory } from 'react-router-dom';
 import AppContext from '../../context/AppContext';
 
 import './NavMenu.scss'
 
-const NavMenu = ({handleState}) => {
+const NavMenu = ({ handleState }) => {
     const { state } = useContext(AppContext);
     const { pages } = state;
 
-    const handleClick = () => {
+    const location = useLocation();
+    const history = useHistory();
+
+    const handleClick = (route) => {
         handleState();
+        history.push(route);
+    }
+
+    const handleLink = (route) => {
+        return location.pathname !== route;
     }
 
     return (
         <div className="NavMenu">
-            {pages.map((page) => (
-                <Link key={page.id} to={page.route} onClick={handleClick} className="NavMenu-item">
-                    <h3>
-                        {page.name}
-                    </h3>
-                    {page.type === "soon" ? 
-                    <span className="NavMenu-soon">
-                        Soon
-                    </span> 
-                    : ''}
-                </Link>
-            ))}
+            {pages.map((page) => {
+                if (handleLink(page.route)) {
+                    return (
+                        <div
+                            key={page.id}
+                            onClick={(e) => { handleClick(page.route) }}
+                            className="NavMenu-item">
+                            <h3>
+                                {page.name}
+                            </h3>
+                            {page.type === "soon" ?
+                                <span className="NavMenu-soon">
+                                    Soon
+                                </span>
+                                : ''}
+                        </div>)
+                } else {
+                    return (
+                        <div
+                            key={page.id}
+                            className="NavMenu-item">
+                            <h3>
+                                {page.name}
+                            </h3>
+                            {page.type === "soon" ?
+                                <span className="NavMenu-soon">
+                                    Soon
+                                </span>
+                                : ''}
+                        </div>)
+                }
+            })}
+
         </div>
     );
 }
